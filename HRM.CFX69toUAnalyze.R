d12c6430b365aaa5b301d2ce244a337013c93ffe
Detestor Homini <amehell@hotmail.com>
2017-06-05 17:51:44 -0500

    Update HRM.CFX69toUAnalyze.R

--- HRM.CFX69toUAnalyze.R
+++ HRM.CFX69toUAnalyze.R
@@ -1,18 +1,25 @@
-HRM.CFX69toUAnalyze = function(x){
+
+
+#This self made script is intended for Melting Amplification Curve File
+#exported directly from BIORAD'S CFX MANAGER v3.0 (or lesser)
+#and to use it on https://dna.utah.edu/uv/uanalyze.html
+
+HRM.CFX96toUAnalyze = function(x){
 	tabla<-read.csv(file=x, header=TRUE,sep=",")
-# Lectura de la extension de la tabla	
+# Read table extension
 	col_a<-dim(tabla)[2] 
 	col_b<-which(colnames(tabla)=="Temperature")+1
 
-# Despues separa las tablas de interes
+# Separate columns
 
 	temp<-tabla$Temperature
 	melt<-tabla[,col_a:col_b]
-# Escribe los archivos .mlt y .tem
+	
+# write mlt and tem files
 
 	write.table(temp, file="temperature.tem", col.names=FALSE, row.names=FALSE, sep="\t", quote=FALSE)
 	write.table(melt, file="melt_matrix.mlt", col.names=FALSE, row.names=FALSE, sep="\t", quote=FALSE)
-# Fin de la minifuncion
+# Remove vars employed
 rm(col_a,col_b,temp,melt)
 
 } 
